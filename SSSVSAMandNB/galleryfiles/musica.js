58184a5064d5651a4293151a71c790f136f8ccd0
tinku21 <46921895+tinku21@users.noreply.github.com>
2021-07-28 18:28:12 +0530

    new gallery

---
+++ SSSVSAMandNB/galleryfiles/musica.js
@@ -0,0 +1,277 @@
+var musica = {
+
+    '/' : [
+         {
+            name: 'Gallery',
+            type: 'folder'
+        },
+       
+    ],
+
+    '/AC-DC - The Very Best': [
+        {
+            name: '..',
+            type: 'folder'
+        },
+        {
+            name: '01.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '02.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '03.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '04.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '05.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '06.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '07.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '08.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '09.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '10.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '11.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '12.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '13.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '14.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '15.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '16.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '17.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '18.jpg',
+            type: 'jpg'
+        },
+        {
+            name: 'Front.jpg',
+            type: 'jpg'
+        },
+        {
+            name: 'CD.jpg',
+            type: 'jpg'
+        },
+        {
+            name: 'Back.jpg',
+            type: 'jpg'
+        }
+    ],
+
+    '/Gallery': [
+        {
+            name: '..',
+            type: 'folder'
+        },
+        {
+            name: 'Disc One',
+            type: 'folder'
+        },
+        {
+            name: 'Disc Two',
+            type: 'folder'
+        },
+        {
+            name: 'Disc Three',
+            type: 'folder'
+        },
+    ],
+
+    '/Gallery/Disc One': [
+        {
+            name: '..',
+            type: 'folder'
+        },
+        {
+            name: '01.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '02.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '03.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '04.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '05.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '06.Fuel.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '07.One.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '08.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '09.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '10.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '11.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '12.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '13.jpg',
+            type: 'jpg'
+        }
+    ],
+
+    '/Gallery/Disc Two': [
+        {
+            name: '..',
+            type: 'folder'
+        },
+        {
+            name: '01.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '2.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '03.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '04.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '05.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '06.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '07.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '08.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '09.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '10.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '11.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '12.jpg',
+            type: 'jpg'
+        }
+    ],
+
+    '/Gallery/Disc Three': [
+        {
+            name: '01.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '02.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '03.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '04.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '05.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '06.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '07.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '08.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '09.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '10.jpg',
+            type: 'jpg'
+        }
+    ]
+}
