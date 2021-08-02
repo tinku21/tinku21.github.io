b4a532a097b2a3d79ac589aa046730714f288249
tinku21 <46921895+tinku21@users.noreply.github.com>
2021-08-02 18:21:53 +0530

    new

--- SSSVSAMandNB/musica.js
+++ SSSVSAMandNB/musica.js
@@ -8,117 +8,23 @@ var musica = {
        
     ],
 
-    '/AC-DC - The Very Best': [
-        {
-            name: '..',
-            type: 'folder'
-        },
-        {
-            name: '01.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '02.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '03.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '04.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '05.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '06.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '07.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '08.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '09.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '10.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '11.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '12.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '13.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '14.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '15.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '16.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '17.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '18.jpg',
-            type: 'jpg'
-        },
-        {
-            name: 'Front.jpg',
-            type: 'jpg'
-        },
-        {
-            name: 'CD.jpg',
-            type: 'jpg'
-        },
-        {
-            name: 'Back.jpg',
-            type: 'jpg'
-        }
-    ],
-
     '/Gallery': [
         {
             name: '..',
             type: 'folder'
         },
         {
-            name: 'Disc One',
+            name: '2021',
             type: 'folder'
         },
         {
-            name: 'Disc Two',
+            name: '2020',
             type: 'folder'
         },
-        {
-            name: 'Disc Three',
-            type: 'folder'
-        },
+       
     ],
 
-    '/Gallery/Disc One': [
+    '/Gallery/2021': [
         {
             name: '..',
             type: 'folder'
@@ -135,49 +41,10 @@ var musica = {
             name: '03.jpg',
             type: 'jpg'
         },
-        {
-            name: '04.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '05.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '06.Fuel.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '07.One.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '08.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '09.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '10.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '11.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '12.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '13.jpg',
-            type: 'jpg'
-        }
+      
     ],
 
-    '/Gallery/Disc Two': [
+    '/Gallery/2020': [
         {
             name: '..',
             type: 'folder'
@@ -228,55 +95,12 @@ var musica = {
         },
         {
             name: '12.jpg',
-            type: 'jpg'
+            type: 'jpg',
+			const image = document.createElement('img'),
+			image.src  = '1.jpg'
+       
         }
     ],
 
-    '/Gallery/Disc Three': [
-	 {
-            name: '..',
-            type: 'folder'
-        },
-        {
-            name: '01.jpg',
-            type: 'jpg',
-			path: "assets/images/kumarsai.png",
-        },
-        {
-            name: '02.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '03.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '04.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '05.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '06.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '07.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '08.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '09.jpg',
-            type: 'jpg'
-        },
-        {
-            name: '10.jpg',
-            type: 'jpg'
-        }
-    ]
+    
 }
