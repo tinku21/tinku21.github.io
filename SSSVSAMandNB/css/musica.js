9ee1071f812101774e2d151eaac6695a6f43dd59
tinku21 <46921895+tinku21@users.noreply.github.com>
2021-08-19 20:25:48 +0530

    new

---
+++ SSSVSAMandNB/css/musica.js
@@ -0,0 +1,106 @@
+var musica = {
+
+    '/' : [
+         {
+            name: 'Gallery',
+            type: 'folder'
+        },
+       
+    ],
+
+    '/Gallery': [
+        {
+            name: '..',
+            type: 'folder'
+        },
+        {
+            name: '2021',
+            type: 'folder'
+        },
+        {
+            name: '2020',
+            type: 'folder'
+        },
+       
+    ],
+
+    '/Gallery/2021': [
+        {
+            name: '..',
+            type: 'folder'
+        },
+        {
+            name: '01.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '02.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '03.jpg',
+            type: 'jpg'
+        },
+      
+    ],
+
+    '/Gallery/2020': [
+        {
+            name: '..',
+            type: 'folder'
+        },
+        {
+            name: '01.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '2.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '03.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '04.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '05.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '06.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '07.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '08.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '09.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '10.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '11.jpg',
+            type: 'jpg'
+        },
+        {
+            name: '12.jpg',
+            type: 'jpg',
+			const image = document.createElement('img'),
+			image.src  = '1.jpg'
+       
+        }
+    ],
+
+    
+}
