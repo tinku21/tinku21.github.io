4e487f4716f8693538d3bf9d2fee1f23e103bc32
tinku21 <gagan@v2k.in>
2022-10-12 17:39:19 +0530

    tes

--- tarot/sender.php
+++ tarot/sender.php
@@ -7,8 +7,8 @@
  **************************************
  */
 
-$to = 'YOUR_EMAIL_ADDRESS';
-$siteName = "YOUR_SITE_NAME";
+$to = 'poojatarotreader@outlook.com';
+$siteName = "pooja kataria";
 
 /*
  *************************************************************
